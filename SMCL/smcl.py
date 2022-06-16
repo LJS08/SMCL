3f4ba2a2c1dd68c969f1397fc61416fe1e32699d
Sharll <10898970+SMDCcreate@user.noreply.gitee.com>
2022-06-16 02:01:25 +0000

    update SMCL/smcl.py.

--- SMCL/smcl.py
+++ SMCL/smcl.py
@@ -1,7 +1,11 @@
 # coding:utf-8
 # Hello, world!
-
-
+import tkinter as tk
 import os
+window = tk.Tk()
+window.title('SMCL')  # 更改标题名字
+window.geometry('960x540')  
+window.iconphoto(False, tk.PhotoImage(file='stone.png'))
+window.mainloop()
 if(os.path.exists('./.smcl')==False):
     os.makedirs('./.smcl')     #创建游戏目录
