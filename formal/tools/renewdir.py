d69d567740523f85723ba1fdaaa7dea7b83a8a37
LJS80 <7722660+ljs80@user.noreply.gitee.com>
2022-07-25 05:13:53 +0000

    update formal/tools/renewdir.py.

--- formal/tools/renewdir.py
+++ formal/tools/renewdir.py
@@ -3,13 +3,13 @@ basic='./.minecraft'
 tasks=["","/assets","/config","/libraries","/logs","/resourcepacks","/saves","/screenshots","/versions"]
 def renew(dir):
     global basic
-    if(os.path.exists(basic+dir)==False):
+    if not os.path.exists(basic+dir):
         os.makedirs(basic+dir)
 count=0
 length = len(tasks)-1
 while True:
     renew(tasks[count])
-    if(count==length):
+    if(count == length):
         break
-    count=count+1
+    count = count+1
 print("ok")
