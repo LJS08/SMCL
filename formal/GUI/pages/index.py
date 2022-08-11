9365a5b4bc02d43cb3497d5904cefe45af9fe2b1
Sharll <10898970+SMDCcreate@user.noreply.gitee.com>
2022-08-11 12:21:52 +0000

    update formal/GUI/pages/index.py.
    Signed-off-by: Sharll <10898970+SMDCcreate@user.noreply.gitee.com>

--- formal/GUI/pages/index.py
+++ formal/GUI/pages/index.py
@@ -4,10 +4,15 @@ import tkinter as tk
 from webbrowser import open
 from PIL import Image, ImageTk
 
-
 def index(root: tk.Tk):
     def open_community():
-        open("https://kaihei.co/JsoIvP")
+        open("https://kook.top/9ccRg6")
+
+    def download_page():
+        return 0 #Nothing now#
+
+    def launch():
+        return 0 #Nothing now#
 
     pixel = tk.PhotoImage(width=1, height=1)
     root.iconphoto(False, tk.PhotoImage(file='./image/icon.png'))
@@ -15,22 +20,34 @@ def index(root: tk.Tk):
     image = ImageTk.PhotoImage(Image.open("./image/bg.png"))
     bg = tk.Label(root, image=image)
     # 标题区域
-    titles_Frame = tk.Frame(root, width=640, height=100, bg="#00AFF0")
-    title = tk.Label(titles_Frame, text="SMCL", bg="#00AFF0", font=("Bahnschrift", 30,))
-    version = tk.Label(titles_Frame, text="Dev 0.2", bg="#00AFF0", font=("Bahnschrift", 15,))
+    titles_Frame = tk.Frame(root, width=800, height=60, bg="#FFFFFF")
+    title = tk.Label(titles_Frame, text="SMCL", bg="#FFFFFF", font=("Bahnschrift", 30,))
     # 官方论坛按钮
-    community_Button = tk.Button(root, text="Community\n----\n官方论坛", image=pixel, bg="#A2C7E8",
+    community_Button = tk.Button(root, text="官方论坛", image=pixel, bg="#A2C7E8",
                                  activebackground="#CADFF2",
-                                 width=160, height=96, bd=0,
-                                 font=("simhei", 12,), command=open_community, compound="c")
+                                 width=120, height=57., bd=0,
+                                 font=("simhei", 14,), command=open_community, compound="c")
+
+    download_page_Button = tk.Button(root, text="下载游戏", image=pixel, bg="#A2C7E8",
+                                     activebackground="#CADFF2",
+                                     width=120, height=57., bd=0,
+                                     font=("simhei", 14,), command=download_page, compound="c")
+
+    launch_Button = tk.Button(root, text="启动游戏", image=pixel, bg="#A2C7E8",
+                                     activebackground="#CADFF2",
+                                     width=150, height=57., bd=0,
+                                     font=("simhei", 14,), command=launch, compound="c")
+
     # 渲染背景
     bg.pack()
     # 渲染标题区域
     titles_Frame.place(x=0, y=0)
-    title.place(x=20, y=10)
-    version.place(x=20, y=55)
+    title.place(x=10, y=5)
     # 渲染官方论坛按钮
-    community_Button.place(x=640, y=0)
-
+    community_Button.place(x=680, y=0)
+    # 渲染下载页面跳转按钮
+    download_page_Button.place(x=0, y=60)
+    # 渲染启动按钮
+    launch_Button.place(x=650, y=390)
     root.mainloop()
     return "okay."
