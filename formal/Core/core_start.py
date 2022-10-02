4b501d13445c5586d047cb1b6991958658efc28c
LJS80 <7722660+ljs80@user.noreply.gitee.com>
2022-10-02 22:48:51 +0800

    fix 无法启动过低版本

--- formal/Core/core_start.py
+++ formal/Core/core_start.py
@@ -1121,6 +1121,10 @@ def core_start_IN(java_path, mc_path, launcher_name, username, uuid_val, aT, lau
 		launcher_uuid_uuid = uuid.uuid4()
 		launcher_uuid = launcher_uuid_uuid.hex
 		uuid_val = launcher_uuid
+
+	global forge_things_g
+	forge_things_g = None
+
 	if start_json["mainClass"] == "net.minecraft.client.main.Main":
 
 		if _Check_versions_in(launcher_name) < _Check_versions_in("1.8.0"):		# 这是低于1.8.0的解决方案
@@ -1236,7 +1240,50 @@ def core_start_IN(java_path, mc_path, launcher_name, username, uuid_val, aT, lau
 			return_IN_list = []
 			return return_IN_list.append("ok", temp_2 + temp_3)
 		else:
+			return_IN_list = []
 			return return_IN_list.append("ok", temp_2 + temp_3, launcher_uuid)
 
+	elif start_json["mainClass"] == "net.minecraft.launchwrapper.Launch":
+		if _Check_versions_in(launcher_name) < _Check_versions_in("1.8.0"):  # 这是低于1.8.0的解决方案
+			for downloads_artifact_inlib in downloads_artifact_inlib_list:
+				temp_2 = temp_2 + (os.path.join(lib_path, (downloads_artifact_inlib.replace("/", "\\")) + ";"))
+			print(downloads_artifact_inlib)
+			the_temp = "\\"
+			temp_3 = client_jar_path + the_temp + gameversion + ".jar" + " net.minecraft.launchwrapper.Launch" + " --username " + username + " --version " + gameversion + " --gameDir " + mc_path + \
+					 " --assetsDir " + assets_index_path + \
+					 " --assetIndex " + assets_index_name + \
+					 " --uuid " + uuid_val + \
+					 " --accessToken " + aT + \
+					 " --userProperties {}" + \
+					 " --userType Legacy" + \
+					 ' --versionType ' + launcher_name_self + \
+					 " --width 854" + \
+					 " --height 480"
+			os.system(temp_2 + temp_3)
+			if uuid_yn:
+				return "ok", temp_2 + temp_3
+			else:
+				return "ok", temp_2 + temp_3, launcher_uuid
 
-core_start_Login(None, None, True, True, "M.R3_BAY")
+		else:
+			for downloads_artifact_inlib in downloads_artifact_inlib_list:
+				temp_2 = temp_2 + (os.path.join(lib_path, (downloads_artifact_inlib.replace("/", "\\")) + ";"))
+			print(downloads_artifact_inlib)
+			the_temp = "\\"
+			temp_3 = client_jar_path + the_temp + gameversion + ".jar" + " net.minecraft.launchwrapper.Launch" + " --username " + username + " --version " + gameversion + " --gameDir " + mc_path + \
+					 " --assetsDir " + assets_index_path + \
+					 " --assetIndex " + assets_index_name + \
+					 " --uuid " + uuid_val + \
+					 " --accessToken " + aT + \
+					 " --userType Legacy" + \
+					 ' --versionType ' + launcher_name_self + \
+					 " --width 854" + \
+					 " --height 480"
+			os.system(temp_2 + temp_3)
+			if uuid_yn:
+				return "ok", temp_2 + temp_3
+			else:
+				return "ok", temp_2 + temp_3, launcher_uuid
+
+
+print(core_start_IN("java", "D:\\HMCL\\.minecraft","1.0", "LJS80", "NONE", "NANE", "1.0", True))
