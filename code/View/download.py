e01dd8cbbd3f3982af35181839719103c3d6bd3e
LJS80 <7722660+ljs80@user.noreply.gitee.com>
2022-12-05 14:49:51 +0000

    update code/View/download.py.
    Signed-off-by: LJS80 <7722660+ljs80@user.noreply.gitee.com>

--- code/View/download.py
+++ code/View/download.py
@@ -1,14 +1,20 @@
 import Core.core_start as core
 import Core.config as config
 import tkinter as tk
+import tkinter.messagebox
 import requests
+import threading
 
 
 def download_page():
     def download():
         dotmc = config.read()[".mc"]
-        core.core_bootstrap_main(True, dotmc, versions.get(versions.curselection()), "BMCLAPI")
+        thread = threading.Thread(target=core.core_bootstrap_main, args=(True, dotmc, versions.get(versions.curselection()), "BMCLAPI"))
+        thread.start()
+        tk.messagebox.showinfo("SMCL", "正在下载，这可能需要一段时间")
+        # core.core_bootstrap_main(True, dotmc, versions.get(versions.curselection()), "BMCLAPI")
         print("Ok")
+        # tk.messagebox.showinfo("SMCL", "下载完毕")
 
     window = tk.Tk()
     window.geometry("640x360")
@@ -18,7 +24,7 @@ def download_page():
     versions = tk.Listbox(window, selectmode=tk.SINGLE)
     btn_ok = tk.Button(window, text="下载", command=download)
 
-    for i in requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").json()["versions"]:
+    for i in core.core_Get_Version_list("release", "BMCLAPI"):
         versions.insert("end", i['id'])
 
     versions.place(x=0, y=0, height=360)
