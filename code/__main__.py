1d9a1ccd02ab3592f7b040110272a9349fa887a3
LJS80 <7722660+ljs80@user.noreply.gitee.com>
2022-12-05 14:52:45 +0000

    update code/__main__.py. 更新入口代码
    Signed-off-by: LJS80 <7722660+ljs80@user.noreply.gitee.com>

--- code/__main__.py
+++ code/__main__.py
@@ -1,10 +1,15 @@
 import View.main
 from Core.mkdir import *
 from loguru import logger
-
+import Core.config
+#nuit_imgpack_src
 logger.add('log/smcl_{time}.log', rotation="50 MB", compression='zip', encoding='utf-8')
 
 if __name__ == "__main__":
     f = Files()
     f.make_mc_dir(".")
-    View.main.main_page()
+    game_dir = os.getcwd()
+    game_dir = os.path.join(game_dir, ".minecraft")
+    Core.config.write(dotmc = game_dir)
+    View.main.main_page(os.path.dirname(__file__))
+    
