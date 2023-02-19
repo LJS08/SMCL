f7a9152e93dc17e1d1f4fef2d6e9be82ce581700
LJS80 <7722660+ljs80@user.noreply.gitee.com>
2023-02-19 04:39:40 +0000

    update formal/main.py. 更新dev命令行测试内容
    Signed-off-by: LJS80 <7722660+ljs80@user.noreply.gitee.com>

--- formal/main.py
+++ formal/main.py
@@ -6,13 +6,15 @@ import Core.config
 logger.add('log/smcl_{time}.log', rotation="50 MB", compression='zip', encoding='utf-8')
 
 if __name__ == "__main__":
+    console_check = False
     f = Files()
     f.make_mc_dir(".")
     game_dir = os.getcwd()
     game_dir = os.path.join(game_dir, ".minecraft")
     Core.config.write(dotmc = game_dir)
-    console_check = False
+    
     if "-dev-console" in sys.argv:
+    import Console.console_main
         Console.console_main.Consoles()
         console_check = True
     
